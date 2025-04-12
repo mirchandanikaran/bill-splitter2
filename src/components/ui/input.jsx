9af4ebae0532102e43c3147e76bd5379567352a7
Karan Mirchandani <85918591+mirchandanikaran@users.noreply.github.com>
2025-04-12 14:19:57 +0530

    Update input.jsx

--- src/components/ui/input.jsx
+++ src/components/ui/input.jsx
@@ -1,6 +1,6 @@
 export const Input = (props) => (
   <input
-    className="border p-2 rounded w-full bg-white text-black dark:bg-zinc-800 dark:text-white"
+    className="border p-2 rounded w-full !bg-white !text-black dark:!bg-black dark:!text-white"
     {...props}
   />
 );
